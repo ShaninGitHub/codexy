4b95ac9f306b27a28fc8ac853583708875fffdb7
Sreenaraj Kinathy <sreenaraj.kinathy@synapse.com>
2017-07-04 02:12:22 +0530

    json code added

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res){
-res.send("Hellow from Server")
-});
+app.use(express.static(__dirname+"/"));
 
 app.listen(3000);
 console.log("Server running on port 3000");
